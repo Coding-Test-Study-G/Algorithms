f098c5e6ed91c98d1a3934898f5d72f292970387
piosj <yeppyteen2@naver.com>
2026-07-30 11:51:14 +0900

    올바른괄호.py

--- 표/stack_queue_heap/올바른괄호.py
+++ 표/stack_queue_heap/올바른괄호.py
@@ -1,6 +1,6 @@
 """
 문제: '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 
-올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+올바르지 않은 괄호이면 false를 return.
 """
 
 def solution(s):
