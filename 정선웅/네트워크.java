fec0a6b59aa2dd76416dadb535d0ff7ddbe7b0d3
jeongseonwoong <tjsdnd1219@naver.com>
2026-08-04 08:58:11 +0900

    Day7) 네트워크.java

--- 정선웅/네트워크.java
+++ 정선웅/네트워크.java
@@ -1,2 +1,28 @@
-public class 네트워크 {
+import java.util.*;
+
+class Solution {
+
+    boolean[] visited;
+    int count = 0;
+
+    public void dfs(int node, int[][] computers) {
+        visited[node] = true;
+        for(int i=0; i<computers[node].length; i++){
+            if(computers[node][i] == 1 && !visited[i]){
+                dfs(i, computers);
+            }
+        }
+    }
+
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[computers.length];
+
+        for(int i=0; i<computers.length; i++) {
+            if(!visited[i]) {
+                dfs(i, computers);
+                count++;
+            }
+        }
+        return count;
+    }
 }
