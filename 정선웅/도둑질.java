c427ca4e530af8d4b3340b170ecd2d18b3b0f91f
jeongseonwoong <tjsdnd1219@naver.com>
2026-09-05 20:03:52 +0900

    도둑질.java

--- 정선웅/도둑질.java
+++ 정선웅/도둑질.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+class Solution {
+    int[] dp1;
+    int[] dp2;
 
-public class 도둑질 {
+    public int solution(int[] money) {
+        int max = 0;
+
+        dp1 = new int[money.length];
+        dp2 = new int[money.length];
+
+        dp1[0] = money[0];
+        dp1[1] = money[1];
+        dp1[2] = dp1[0] + money[2];
+
+        for(int i=3; i<money.length-1; i++) {
+            dp1[i] = money[i] + Math.max(dp1[i-2], dp1[i-3]);
+        }
+
+        dp2[0] = 0;
+        dp2[1] = money[1];
+        dp2[2] = dp2[0] + money[2];
+
+        for(int i=3; i<money.length; i++) {
+            dp2[i] = money[i] + Math.max(dp2[i-2], dp2[i-3]);
+        }
+
+        for(int i=0; i<dp1.length; i++) {
+            max = Math.max(max,dp1[i]);
+        }
+        for(int i=0; i<dp1.length; i++) {
+            max = Math.max(max,dp2[i]);
+        }
+
+        return max;
+    }
 }
