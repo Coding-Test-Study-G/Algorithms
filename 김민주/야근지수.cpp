0e7be9c960b5d495ff90df5df8e10daf7c76e2b2
Minju Kim <105335065+mjk25@users.noreply.github.com>
2026-09-22 00:31:19 +0900

    Create 야근지수.cpp

---
+++ 김민주/야근지수.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+    long long answer = 0;
+    priority_queue <int> pq;
+    for (int i=0; i<works.size(); i++){
+        pq.push(works[i]);
+    }
+    int j = n;
+    while(!pq.empty() && j>0){
+        int node = pq.top();
+        pq.pop();
+        if (node >= 1){
+            node -= 1;
+            j -= 1;
+            pq.push(node);
+        }
+    }
+    
+    while(!pq.empty()){
+        int node = pq.top();
+        answer += node * node;
+        pq.pop();
+    }
+    
+    return answer;
+}
